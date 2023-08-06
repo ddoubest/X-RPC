2f513b76d6534a1ddddee47341db6e37ed504062
chike <chike.hjh@taobao.com>
2023-08-06 15:26:15 +0800

    init project structure

--- rpc-example/src/main/java/org/chike/example/learning/zookeeper/Client.java
+++ rpc-example/src/main/java/org/chike/example/learning/zookeeper/Client.java
@@ -1,4 +1,4 @@
-package org.chike.xrpc.learning.zookeeper;
+package org.chike.example.learning.zookeeper;
 
 import org.apache.curator.RetryPolicy;
 import org.apache.curator.framework.CuratorFramework;
@@ -7,8 +7,8 @@ import org.apache.curator.retry.ExponentialBackoffRetry;
 import org.apache.zookeeper.CreateMode;
 
 public class Client {
-    private static final int BASE_SLEEP_TIME= 1000;
-    private static final int MAX_RETRIES= 3;
+    private static final int BASE_SLEEP_TIME = 1000;
+    private static final int MAX_RETRIES = 3;
 
     public static void main(String[] args) throws Exception {
         RetryPolicy retryPolicy = new ExponentialBackoffRetry(BASE_SLEEP_TIME, MAX_RETRIES);
